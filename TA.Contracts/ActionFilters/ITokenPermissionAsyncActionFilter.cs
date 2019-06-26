1f2fe7b83d5864668e1ad76f3ffd3711a88d7ab1
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-06-26 19:18:50 +0100

    Inherits IAsyncActionFilter

--- TA.Contracts/ActionFilters/ITokenPermissionAsyncActionFilter.cs
+++ TA.Contracts/ActionFilters/ITokenPermissionAsyncActionFilter.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace TA.Contracts.ActionFilters
 {
-    public interface ITokenPermissionAsyncActionFilter<TToken, in TPermission>
+    public interface ITokenPermissionAsyncActionFilter<TToken, in TPermission> : IAsyncActionFilter
     {
         Task<bool> IsValid(HttpContext httpContext, TToken token);
         bool HasPermissions(HttpContext httpContext, TToken token , IEnumerable<TPermission> permissions);
