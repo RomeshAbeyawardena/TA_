6d00e234e0619473bb0de885e39de8bca2c4c2a8
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-06-26 19:17:24 +0100

    Added contract to RequireApiKeyAttribute

---
+++ TA.Contracts/ActionFilters/ITokenPermissionAsyncActionFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TA.Contracts.ActionFilters
+{
+    public interface ITokenPermissionAsyncActionFilter<TToken, in TPermission>
+    {
+        Task<bool> IsValid(HttpContext httpContext, TToken token);
+        bool HasPermissions(HttpContext httpContext, TToken token , IEnumerable<TPermission> permissions);
+    }
+}
